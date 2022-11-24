49befb876fdcc99d72711cb7f12b1d1f6e9507b6
Casimir Colas <casimir.colas@gmail.com>
2022-11-24 11:49:09 +0100

    repair

--- hakaton/src/App.js
+++ hakaton/src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import {useEffect,useState} from 'react';
+import MonCard from "./components/MonCard/MonCard"
+import RegCard from "./components/RegCard/RegCard"
 
 function App() {
+  const [search, setSearch] = useState(undefined);
+  function textChange(value) {
+    setSearch(value)
+  }
   return (
     <div className="App">
+      <input type="search" onChange={(e)=>textChange(e.target.value)}/>
+      <p>{search}</p>
     </div>
   );
 }
