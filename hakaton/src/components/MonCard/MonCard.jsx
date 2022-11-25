68ac9a3e63e6dcd7bb97ed21c99941cc983fb3ef
FITSCH YANN <Fitsch-yann@hotmail.fr>
2022-11-25 10:16:50 +0100

    css modif

--- hakaton/src/components/MonCard/MonCard.jsx
+++ hakaton/src/components/MonCard/MonCard.jsx
@@ -5,7 +5,7 @@ import data from '../../data/data';
 function MonCard({ id, onClick }) {
     return (
         <div>
-            <div className='btnmoncard' onClick={onClick}>←</div>
+            <div className='btnmoncard' onClick={onClick}>X</div>
             <div className='monCard'>
                 <h3 className='cardName'>{data.Touristique[id - 1].name}</h3>
                 <img className='imgMonument' src={data.Touristique[id - 1].url} alt="not found" />
