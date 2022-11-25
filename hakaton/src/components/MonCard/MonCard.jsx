2cb5dd0cf3cc4a5b07a01f9fab85a83fd83637c1
FITSCH YANN <Fitsch-yann@hotmail.fr>
2022-11-25 10:02:25 +0100

    add css

--- hakaton/src/components/MonCard/MonCard.jsx
+++ hakaton/src/components/MonCard/MonCard.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import "./moncard.css";
 import data from '../../data/data';
 
-function MonCard({ id,  onClick}) {
-return(
-    <div className='monCard'>
-    <button onClick={onClick}>X</button>
-    <h3>{data.Touristique[id-1].name}</h3>
-    <img src={data.Touristique[id-1].url} alt="not found"/>
-    <p>{data.Touristique[id-1].desc}</p>
-</div>
-)
+function MonCard({ id, onClick }) {
+    return (
+        <div>
+            <div className='btnmoncard' onClick={onClick}>←</div>
+            <div className='monCard'>
+                <h3 className='cardName'>{data.Touristique[id - 1].name}</h3>
+                <img className='imgMonument' src={data.Touristique[id - 1].url} alt="not found" />
+                <p className='cardDesc'>{data.Touristique[id - 1].desc}</p>
+            </div>
+        </div>
+    )
 };
 
 export default MonCard;
