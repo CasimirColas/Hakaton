cbd05371fc4dbdad562d18e31baf4a280ea47aa5
ThibaultLocquet <thibaultlocquetweb@gmail.com>
2022-11-25 11:41:45 +0100

    transfer casimir

--- hakaton/src/components/MicroMonu/MicroMonu.jsx
+++ hakaton/src/components/MicroMonu/MicroMonu.jsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
 import MonCard from "../MonCard/MonCard";
 
-function MicroMonu({id,img,name}) {
-    const [displayMonu, setDisplayMonu] = useState(false);
-    function clickOpen() {
-        if(displayMonu===false){
-            setDisplayMonu(true)
-        }
+function MicroMonu({ id, img, name }) {
+  const [displayMonu, setDisplayMonu] = useState(false);
+  function clickOpen() {
+    if (displayMonu === false) {
+      setDisplayMonu(true);
     }
-    function clickClose() {
-        setDisplayMonu(false)
-    }
-    return (<div className="microMonu" style={{background: `url(${img})`}} onClick={clickOpen}>
-        <h5>{name}</h5>
-        {displayMonu? <MonCard id={id} onClick={clickClose}/>:null}
-    </div>);
+  }
+  function clickClose() {
+    setDisplayMonu(false);
+  }
+  return (
+    <div
+      className="microMonu"
+      style={{ background: `url(${img})`, backgroundSize: "cover" }}
+      onClick={clickOpen}
+    >
+      <h6>{name}</h6>
+      {displayMonu ? <MonCard id={id} onClick={clickClose} /> : null}
+    </div>
+  );
 }
 
 export default MicroMonu;
